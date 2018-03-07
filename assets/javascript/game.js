61885a6746b9d2cf9f582c2dffdd5402591bd19a
joshtecx <32961912+joshtecx@users.noreply.github.com>
2018-03-07 14:29:57 -0500

    functioning crystal  game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,73 @@
 // Randomly generated target number
-var targetNum = Math.floor(Math.random() * 101) + 19;
-console.log(targetNum);
+var targetNum;
 
-$("#goal").text(targetNum);
-
+var wins = 0;
+var losses = 0;
 var crystals = $("#crystals");
 var counter = 0;
 
-var incrementOptions = [1, 3, 6, 9];
+function startGame() {
 
-for (var i = 0; i < incrementOptions.length; i++) {
-    var blueCrystal = $("<img>");
-    // var greenCrystal = $("<img>");
+    $("#crystals").empty();
 
-    // adds class to image
-    blueCrystal.addClass("crystal-image");
-    // greenCrystal.addClass("crystal-image");
+    targetNum = Math.floor(Math.random() * 101) + 19;
+    console.log(targetNum);
+    $("#goal").text(targetNum);
+   
+    for (var i = 0; i < 4; i++) {
 
-    // adds src and link to image
-    blueCrystal.attr("src", "assets/images/blue.png", );
-    // greenCrystal.attr("src", "assets/images/green.png", );
+        var randomIncrement = [Math.floor(Math.random() * 11) + 1];
+        console.log(randomIncrement);
 
-    // adds data attribute to image and 
-    blueCrystal.attr("data-crystalvalue", incrementOptions[i]);
-    // greenCrystal.attr("data-cryatalvalue", incrementOptions[i]);
+        var blueCrystal = $("<img>");
+        // var greenCrystal = $("<img>");
 
-    // adds crystal to the page
-    crystals.append(blueCrystal);
-    // crystals.append(greenCrystal);
+        // adds class to image
+        blueCrystal.addClass("crystal-image");
+        // greenCrystal.addClass("crystal-image");
+
+        // adds src and link to image
+        blueCrystal.attr("src", "assets/images/blue.png", );
+        // greenCrystal.attr("src", "assets/images/green.png", );
+
+        // adds data attribute to image and 
+        blueCrystal.attr("data-crystalvalue", randomIncrement);
+        // greenCrystal.attr("data-cryatalvalue", incrementOptions[i]);
+
+        // adds crystal to the page
+        crystals.append(blueCrystal);
+        // crystals.append(greenCrystal);
+    }
 }
 
-crystals.on("click", ".crystal-image", function() {
+startGame();
+
+$(document).on("click", ".crystal-image", function() {
     // grabs whatever value is stored within the data-crystalvalue attribute
     var crystalValue  = ($(this).attr("data-crystalvalue"));
     // converts that value to an integer
     crystalValue = parseInt(crystalValue);
     // increments the counter by number stored in crystalValue
     counter += crystalValue;
-
+    // Alerts player of their current score 
     alert("SCORE: " + counter);
-
+    // displays win/loss message 
     if (counter === targetNum) {
         alert("You did it! Nice job!");
+        // adds wins 
+        wins++;
+        console.log(wins);
+        $("#wins").text(wins);
+        counter = 0;
+        startGame();
     } else if (counter > targetNum){
         alert("Sorry! Try again!");
+        // adds losses
+        losses++;
+        console.log(losses);
+        $("#losses").text(losses);
+        counter = 0;
+        startGame();
     }
 
 });
